04e922608b925b509c717c42a794f8a4560fec70
Cale Sennett <calesennett@gmail.com>
2014-10-07 21:58:40 -0500

    add vector implementation of event matrix

--- Haskell/HW2.hs
+++ Haskell/HW2.hs
@@ -5,15 +5,20 @@ import qualified Lib.DataParser as DP
 import QSTKUtil.Date
 import Data.List.Split
 import Data.List
+import qualified Data.Vector as V
 
 main =  do
         ps <- spy_syms
         print "Reading data..."
-        p <- mapM (DP.readFrom (parseNYSE "01/01/2008") (parseNYSE "12/31/2009")) ps
+        let sd = "01/01/2008"
+        let ed = "01/31/2008"
+        p <- mapM (DP.readFrom (parseNYSE sd) (parseNYSE ed)) ps
         print "Finding events..."
-        print p
-        let events = map EP.countEvents p
-        print $ sum events
+        --let events = map EP.events p
+        let events = [V.fromList [0]]
+        nyseDays <- (DU.getNYSEdays sd ed)
+        returns <- mapM (EP.returns sd ed nyseDays 10 10 0 ps) events
+        print returns
 
 spy_syms :: IO [String]
 spy_syms =  do
--- Haskell/QSTKStudy/EventProfiler.hs
+++ Haskell/QSTKStudy/EventProfiler.hs
@@ -1,27 +1,40 @@
 module QSTKStudy.EventProfiler
-    ( countEvents
+    ( countEvents,
+      events,
+      returns,
+      dateSpan
     ) where
 
+import qualified Data.Vector as V
+import qualified QSTKUtil.QSTsUtil as TSU
+import qualified Lib.DataParser as DP
+import qualified QSTKUtil.QSDateUtil as DU
+import Data.Time.Calendar
+import Data.Time
 
---countEvents :: [String] -> Integer
---countEvents [] = 0
---countEvents ("nan":_) = 0
---countEvents (x:[]) = 0
---countEvents (x:xs) = if (read x >= 5.0) && (read (head xs) < 5.0)
---                     then 1 + countEvents xs
---                     else 0 + countEvents xs
+pairs xs = zip xs (tail xs)
+
+events :: [String] -> V.Vector Integer
+events xs = V.fromList (0 : (map event $ pairs xs))
+
+--returns (matrixIndex, lookForward, lookBack, events)
+returns :: String -> String -> [Day] -> Integer -> Integer -> Int -> [String] -> V.Vector Integer -> IO [String]
+returns sd ed ds lb lf i syms em =  if (em V.! i == 0)
+                                    then (DP.readFrom (fst (dateSpan ds i lb lf)) (snd (dateSpan ds i lb lf))) "AAPL"
+                                    else return []
+
+-- dateSpan (matrixIndex, lookForward, lookBack)
+dateSpan :: [Day] -> Int -> Integer -> Integer -> (Day, Day)
+dateSpan ds i lb lf = (addDays (- (lb)) $ ds !! i, addDays (lf) $ ds !! i)
 
 countEvents :: [String] -> Integer
 countEvents [] = 0
-countEvents ("nan":_) = 0
 countEvents (x:[]) = 0
-countEvents (x:xs) = if (event (x, head xs))
-                     then 1 + countEvents xs
-                     else 0 + countEvents xs
+countEvents all@(x:xs) = sum (map event $ pairs all)
 
-event :: (String, String) -> Bool
-event (x, "nan") = False
-event ("nan", y) = False
+event :: (String, String) -> Integer
+event (x, "nan") = 0
+event ("nan", y) = 0
 event (x, y) =  if (read x >= 10.0) && (read y < 10.0)
-                then True
-                else False
+                then 1
+                else 0
